5061d0ba7f4f8430f9c5755bdde19f30c14b8ccd
marlon-buckley <m4rlz@live.co.uk>
2022-11-02 17:56:01 +0000

    solution for day3 part1

--- day3/binaryDiagnosticPart1.js
+++ day3/binaryDiagnosticPart1.js
@@ -1,29 +1,56 @@
 // need to use input to calculate the gamma rate and epsilon rate
 const fs = require('fs');
 
-const puzzleInput = fs.readFileSync('./example.txt', 'utf8');
+const puzzleInput = fs.readFileSync('./input.txt', 'utf8');
 const lines = puzzleInput.split(/\n/);
 const test = [8, 7, 8, 8];
 
-function columnsOfBits(arr) {
-  const hashmap = arr.reduce( (acc, val) => {
-   acc[val] = (acc[val] || 0 ) + 1
-   console.log(acc);
-   return acc
-},{})
-console.log(hashmap);
-return Object.keys(hashmap).reduce((a, b) => hashmap[a] > hashmap[b] ? a : b)
+
+//returns all the bits in a given column
+function columnPuller(input, colNumber) { 
+  let column = [];
+  input.forEach((line) => {
+    column += line[colNumber];
+  });
+  return column;
 }
 
+//returns an array where each element is an array of individual bits from each column
+function columnsOfBits(input) { 
+  numOfColumns = input[0].length;
+  let columns = [];
+  for (let i = 0; i < numOfColumns; i++) {
+    columns[i] = columnPuller(input, i).split('');
+  }
+  return columns;
+};
 
-function mostCommonBit(arr) {
+//Found this solution here https://javascript.plainenglish.io/how-to-find-the-most-frequent-element-in-an-array-in-javascript-c85119dc78d2`
+function mostCommonBit(arr, type) {
   const hashmap = arr.reduce( (acc, val) => {
    acc[val] = (acc[val] || 0 ) + 1
-   console.log(acc);
-   return acc
-},{})
-console.log(hashmap);
-return Object.keys(hashmap).reduce((a, b) => hashmap[a] > hashmap[b] ? a : b)
+  //  console.log(acc);
+   return acc;
+  },{})
+  if (type === "gammaRate") {
+    return Object.keys(hashmap).reduce((a, b) => hashmap[a] > hashmap[b] ? a : b)
+  } else if (type === "epsilonRate") {
+    return Object.keys(hashmap).reduce((a, b) => hashmap[a] < hashmap[b] ? a : b)
+  }
 }
 
-console.log(mostCommonBit(test));
+function sorter(input) {
+  const columns = columnsOfBits(input);
+  let gammaRate = [];
+  let epsilonRate = [];
+
+  columns.forEach((column) => {
+    gammaRate += mostCommonBit(column, "gammaRate");
+    epsilonRate += mostCommonBit(column, "epsilonRate");
+  });
+
+  const powerConsumption = parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
+  return ` Gammar rate: ${gammaRate}\n Epsilon rate: ${epsilonRate}\n Power consumption: ${powerConsumption}`;
+};
+
+console.log(sorter(lines));
